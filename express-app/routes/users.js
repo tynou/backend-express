71aec3e8999f73a57b0b5996fc0c5191119dd490
Alexander Timonin <2sdaiwxet@gmail.com>
2026-03-24 17:06:42 +0500

    3

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,9 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
+const users = {
+  items: [
+    {
+      "id": 1,
+      "name": "Шишкина Светлана"
+    },
+    {
+      "id": 2,
+      "name": "Тимонин Александр"
+    }
+  ]
+}
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  res.send(users.items);
 });
 
 module.exports = router;
